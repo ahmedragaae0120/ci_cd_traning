7a8ef23fd994cf7b75d50b6c48c7b52b75d65829
اسمك <ahmedbasha01208@gmail.com>
2025-04-08 18:53:45 +0200

    create linting issue

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class myApp extends StatelessWidget {
+  const myApp({super.key});
 
   @override
   Widget build(BuildContext context) {
